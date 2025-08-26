e02dced6343b3d4ea41c98a86be507ccbd388eb9
shironxiao <sevillaronald32@gmail.com>
2025-08-26 16:26:36 +0800

    addede

--- SevillaRonaldAssignment/Form1.vb
+++ SevillaRonaldAssignment/Form1.vb
@@ -1,19 +1,17 @@
 ﻿Public Class Form1
-
-
-    Private Sub usernameTextBox_TextChanged(sender As Object, e As EventArgs) Handles usernameTextBox.TextChanged
-
+    Private Sub LoginInButton_Click(sender As Object, e As EventArgs) Handles LoginInButton.Click
+        If usernameTextBox.Text = "admin" And passwordTextBox.Text = "1234" Then
+            MessageBox.Show("Login Success!")
+        Else
+            MessageBox.Show("Invalid username or password!")
+        End If
     End Sub
 
     Private Sub passwordTextBox_TextChanged(sender As Object, e As EventArgs) Handles passwordTextBox.TextChanged
 
     End Sub
 
-    Private Sub LoginInButton_Click(sender As Object, e As EventArgs) Handles LoginInButton.Click
-        If usernameTextBox.Text = "admin" And passwordTextBox.Text = "1234" Then
-            MessageBox.Show("Login Success!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        Else
-            MessageBox.Show("Invalid username or password!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        End If
+    Private Sub usernameTextBox_TextChanged(sender As Object, e As EventArgs) Handles usernameTextBox.TextChanged
+
     End Sub
 End Class
